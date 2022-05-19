3388efe91060ac6716b37e8a5a7747338ffe90c3
Brian <bdmorin@gmail.com>
2022-05-19 17:41:10 -0500

    added url var, and link for copying

--- functions/robohash.fish
+++ functions/robohash.fish
@@ -1,11 +1,11 @@
 function robohash
-        ## $argv $I
     set -l outfile 
     set -l term (string escape --style=url "$argv")
     for I in (seq 4);
+        set -l url "https://robohash.org/set_set$I/$term"
         set -a outfile "
-![https://robohash.org/set_set$I/$term](https://robohash.org/set_set$I/$term)
-- $argv set=$I
+![$url]($url)
+- [$url]($url)
 "
     end
     echo $outfile
